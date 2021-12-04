a2a81e890d2fdc9440ae8e023375072cd5261a6e
Maccio92 <valerio362@gmail.com>
2021-12-04 15:21:54 +0100

    add function & css

--- js/script.js
+++ js/script.js
@@ -3,35 +3,72 @@
 Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
 
-// Creo un array con 5 numeri causali
-let numbers = [];
-        while (numbers.length < 5) {
-            let numberRandom = parseInt(Math.floor(Math.random() * 101));
-            if(numbers.includes(numberRandom) != true){
-                numbers.push(numberRandom);
-            } 
-        }
-console.log(numbers);
 
-// Stampo i 5 numeri in pagina
+
+// Funzione: Creo un array con 5 numeri causali
+function randNumbers() {
+    let randomArrayNumbers = [];
+    while (randomArrayNumbers.length < 5) {
+        let numberRandom = parseInt(Math.floor(Math.random() * 101));
+        if(randomArrayNumbers.includes(numberRandom) != true){
+            randomArrayNumbers.push(numberRandom);
+        } 
+    }
+    console.log(randomArrayNumbers);
+    return randomArrayNumbers;
+}
+
+let userNumbers = [];
+let message = '';
+// Funzion per chiedere all'utente di inserire i numeri
+function userInput() {
+    let check = 0; /*Variabile di controllo dei numeri indovinati*/
+            // Chiedo all'utente di inserire i 5 numeri
+            for (let i = 0; i < numbers.length; i++) {
+                let userNum = parseInt(prompt('Inserisci un numero'));
+                // Controllo se il valore inserito è un numero
+                while (isNaN(userNum)) {
+                userNum = parseInt(prompt('Valore non valido. Riprova'));
+                }
+                userNumbers.push(userNum);
+                if (numbers.includes(userNum)) {
+                    check += 1 ; /*Incremento della Variabile di controllo se il numero è presente*/
+                }
+            }
+            console.log(userNumbers);
+            // Controllo quanti numeri sono stati indovinati.
+            if (check == numbers.length) {
+                message = `<p class="text">Hai inserito correttamente tutti i numeri! Complimenti</p>
+                            <p>I numeri da ricordare: ${numbers}</p><br>
+                            <p>I tuoi numeri: ${userNumbers}</p>`;
+            } else {
+                message = `<p class="text">Hai inserito ${check} numeri esatti </p>
+                           <p>I numeri da ricordare: ${numbers}</p><br>
+                           <p>I tuoi numeri: ${userNumbers}</p>`;
+            }
+            console.log (message);
+            let messageDom = document.querySelector ('.message');
+            messageDom.innerHTML = `<h2>${message}</h2>`;
+}
+// Inserimento dei 5 numeri casuali nel DOM
+let numbers = randNumbers(); /*Richiamo la funzione di generazione random*/
 let container = document.querySelector (".container");
 let numberContainer = document.createElement('div');
 numberContainer.append (numbers);
 container.append(numberContainer);
 
-// Creao il countdown di 30 secondi
-let seconds = 10;
-const countdown = setInterval(() => {  
-      if (seconds > 0) {
-        console.log (seconds, 'secondi');
-        seconds -= 1;
-      } else{
-        clearInterval(countdown);
-        container.innerHTML ="";}
-      }, 1000);
-      
+let seconds = 5;
+setTimeout(() => {
+    container.innerHTML = '';   
+    // Creo il countdown di 30 secondi
+    const timer = setInterval(() => {
+        if (seconds > 0) {
+            console.log (seconds, 'secondi');
+            seconds -= 1;
+        } else {
+            clearInterval(timer);
+            userInput(); /*Inserisco la funzione per chiedere numeri*/
+        }
+    }, 1000);
+}, 3000); /*Tempo di visualizzazione dei 5 numeri random*/
 
-
-
-
-    //   5 prompt
